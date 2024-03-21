01ee8d2db3a0458f3c863ba1c7ac55647a50c453
myrto <myrto997@gmail.com>
2024-03-21 18:13:21 +0200

    WIP 20 - DidIssuer class: supports web and key did creation - KeyCreation: generates keys with different allgorithms and character length. - fixed library versioning - Export key as a jwk functionality via the KeyJWkExport

--- provenai-sdk/src/main/java/dev/ctrlspace/provenai/ssi/KeyCreation.java
+++ provenai-sdk/src/main/java/dev/ctrlspace/provenai/ssi/KeyCreation.java
@@ -8,12 +8,12 @@ import kotlin.coroutines.Continuation;
 
 public class KeyCreation {
 
-    public static LocalKey generateKey(KeyType keyType) {
+    public static LocalKey generateKey(KeyType keyType, int characterLength) {
         // Create a continuation object
         Continuation<? super Object> continuation = ContinuationObjectUtils.createSuperContinuation();
 
         // Create an instance of LocalKeyMetadata
-        LocalKeyMetadata metadata = new LocalKeyMetadata();
+        LocalKeyMetadata metadata = new LocalKeyMetadata(characterLength);
 
         // Generate a key using the generate function from the companion object
         return (LocalKey) LocalKey.Companion.generate(keyType, metadata, continuation);
