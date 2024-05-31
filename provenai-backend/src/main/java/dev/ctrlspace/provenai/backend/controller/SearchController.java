48ab79272437f05dbac0360f5a019c919d9e814e
myrto <myrto997@gmail.com>
2024-05-31 12:21:47 +0300

    Feature 9: Agent Registration Feature 10: Search API

--- provenai-backend/src/main/java/dev/ctrlspace/provenai/backend/controller/SearchController.java
+++ provenai-backend/src/main/java/dev/ctrlspace/provenai/backend/controller/SearchController.java
@@ -21,4 +21,30 @@ public class SearchController {
         return List.of();
     }
 
+//    @PostMapping("/search")
+//    public List<SearchResult> search(@RequestParam String question, Authentication authentication) {
+//        // find data pod IDs whose ACL policies match with Agents' policies
+//        // get Data Pods ids
+//        // REST call to gendox -> semantic search in data Pods IDs
+//        // prepare Permission of use VC, with sections ISCC
+//
+//        UUID agentID = UUID.fromString("3432-kj453-534kl56-65b4");
+//
+//        // match agent's policies with data pods' policies
+////        1. GET all Pods that have AgentID in the Allow List
+////        2. GET all Pods that have 'Pod Permission to Use' eg Education == 'Agent's  Purpose to Use' eg Education
+////        3. REMOVE all Pods that have AgentID in the Block List
+////      List<DataPod> dataPods = dataPodService.getDataPods(agentID);
+//
+////        get list of podIDs from dataPods (per hostURL) which are the Gendox project Ids eg ids = ["424235", "65tg54", "654546"]
+//
+//        // Do semantic search with criteria projectIdIn = ids
+//
+////        Get the results and prepare the [Responce DTO, text, ISCC, list of Usage Permition, owner name], etc
+//
+////        return the results
+//
+//        return List.of();
+//    }
+
 }
