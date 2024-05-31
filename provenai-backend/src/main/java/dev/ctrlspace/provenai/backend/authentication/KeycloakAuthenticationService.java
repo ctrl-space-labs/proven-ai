1bd59cd3aac5cdb185a45d127502cab495281b03
sekasx <sekas.x@gmail.com>
2024-05-31 14:59:06 +0300

    WIP #26 Setup Keycloal as IDP
    - Add KeycloakService method to get admin jwt credentials
    - Add Gendox UserProfile adapter to get users profile through API call along with all supporting classes like OrganizationUserDTO, JwtDTO etc
    - Add AuthenticationService interface
    - Add JWTProfile filter to construct users JWT from the JWT
    - Update env properties

--- provenai-backend/src/main/java/dev/ctrlspace/provenai/backend/authentication/KeycloakAuthenticationService.java
+++ provenai-backend/src/main/java/dev/ctrlspace/provenai/backend/authentication/KeycloakAuthenticationService.java
@@ -4,10 +4,14 @@ import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.Payload;
 import com.nimbusds.jwt.JWTParser;
 import com.nimbusds.jwt.SignedJWT;
+import dev.ctrlspace.provenai.backend.exceptions.ProvenAiException;
+import dev.ctrlspace.provenai.backend.model.authentication.UserProfile;
+import org.jetbrains.annotations.Nullable;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.AccessTokenResponse;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +20,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -24,116 +29,142 @@ import org.springframework.web.client.RestTemplate;
 import java.text.ParseException;
 import java.time.Instant;
 import java.util.Map;
+import java.util.Optional;
 
 @Component
-public class KeycloakAuthenticationService {
+public class KeycloakAuthenticationService implements AuthenticationService {
 
 
-        private Logger logger = LoggerFactory.getLogger(KeycloakAuthenticationService.class);
+    private Logger logger = LoggerFactory.getLogger(KeycloakAuthenticationService.class);
 
-        private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();
 
-        private String keycloakServerUrl;
+    private String keycloakServerUrl;
 
-        private String keycloakTokenUrl;
+    private String keycloakTokenUrl;
 
-        private String realm;
+    private String realm;
 
-        private String clientId;
+    private String clientId;
 
-        private String clientSecret;
+    private String clientSecret;
 
-        private Keycloak keycloakClient;
+    private Keycloak keycloakClient;
+
+    private JwtDecoder jwtDecoder;
+
+    public KeycloakAuthenticationService(@Value("${keycloak.base-url}") String keycloakServerUrl,
+                                         @Value("${keycloak.token-uri}") String keycloakTokenUrl,
+                                         @Value("${keycloak.realm}") String realm,
+                                         @Value("${keycloak.client-id}") String clientId,
+                                         @Value("${keycloak.client-secret}") String clientSecret,
+                                         JwtDecoder jwtDecoder) {
+
+        this.keycloakServerUrl = keycloakServerUrl;
+        this.keycloakTokenUrl = keycloakTokenUrl;
+        this.realm = realm;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+
+        keycloakClient = KeycloakBuilder.builder()
+                .serverUrl(keycloakServerUrl)
+                .realm(realm)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
+
+    }
+
+    @Override
+    public Jwt getClientToken() {
+        String jwtString = getClientTokenString();
+        Jwt jwt = jwtDecoder.decode(jwtString);
+        return jwt;
+    }
+
+    @Override
+    public String getClientTokenString() {
+        String jwtString = keycloakClient.tokenManager().getAccessTokenString();
+        return jwtString;
+    }
+
+    public Jwt impersonateUser(String username) {
+
+        AccessTokenResponse clientToken = keycloakClient.tokenManager().getAccessToken();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", OAuth2Constants.TOKEN_EXCHANGE_GRANT_TYPE);
+        map.add("client_id", clientId);
+        map.add("client_secret", clientSecret);
+        map.add("subject_token", clientToken.getToken());
+        map.add("requested_subject", username);
+        map.add("requested_token_type", OAuth2Constants.ACCESS_TOKEN_TYPE);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
 
 
-        public KeycloakAuthenticationService(@Value("${keycloak.base-url}") String keycloakServerUrl,
-                                             @Value("${keycloak.token-uri}") String keycloakTokenUrl,
-                                             @Value("${keycloak.realm}") String realm,
-                                             @Value("${keycloak.client-id}") String clientId,
-                                             @Value("${keycloak.client-secret}") String clientSecret) {
+        ResponseEntity<AccessTokenResponse> impersonationToken = restTemplate.postForEntity(
+                keycloakTokenUrl,
+                request,
+                AccessTokenResponse.class);
 
-                this.keycloakServerUrl = keycloakServerUrl;
-                this.keycloakTokenUrl = keycloakTokenUrl;
-                this.realm = realm;
-                this.clientId = clientId;
-                this.clientSecret = clientSecret;
-
-                keycloakClient = KeycloakBuilder.builder()
-                        .serverUrl(keycloakServerUrl)
-                        .realm(realm)
-                        .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
-                        .clientId(clientId)
-                        .clientSecret(clientSecret)
-                        .build();
-
+        String tokenString = impersonationToken.getBody().getToken();
+        // Parse the JWT token
+        SignedJWT signedJWT = null;
+        try {
+            signedJWT = (SignedJWT) JWTParser.parse(tokenString);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
 
-        public Jwt impersonateUser(String username) {
-
-                AccessTokenResponse clientToken = keycloakClient.tokenManager().getAccessToken();
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-                MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-                map.add("grant_type", OAuth2Constants.TOKEN_EXCHANGE_GRANT_TYPE);
-                map.add("client_id", clientId);
-                map.add("client_secret", clientSecret);
-                map.add("subject_token", clientToken.getToken());
-                map.add("requested_subject", username);
-                map.add("requested_token_type", OAuth2Constants.ACCESS_TOKEN_TYPE);
-
-                HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+        // Extract headers and claims
+        JWSHeader jwtHeaders = signedJWT.getHeader();
+        Payload payload = signedJWT.getPayload();
 
 
-                ResponseEntity<AccessTokenResponse> impersonationToken = restTemplate.postForEntity(
-                        keycloakTokenUrl,
-                        request,
-                        AccessTokenResponse.class);
+        // Convert Unix timestamps to Instant
+        Map<String, Object> claims = payload.toJSONObject();
+        convertTimestampsToInstant(claims);
 
-                String tokenString = impersonationToken.getBody().getToken();
-                // Parse the JWT token
-                SignedJWT signedJWT = null;
-                try {
-                        signedJWT = (SignedJWT) JWTParser.parse(tokenString);
-                } catch (ParseException e) {
-                        throw new RuntimeException(e);
-                }
-
-                // Extract headers and claims
-                JWSHeader jwtHeaders = signedJWT.getHeader();
-                Payload payload = signedJWT.getPayload();
+        // Rebuild the token using Spring Boot's Jwt class
+        Jwt jwt = Jwt.withTokenValue(tokenString)
+                .headers(h -> h.putAll(jwtHeaders.toJSONObject()))
+                .claims(c -> c.putAll(claims))
+                .build();
 
 
-                // Convert Unix timestamps to Instant
-                Map<String, Object> claims = payload.toJSONObject();
-                convertTimestampsToInstant(claims);
+        return jwt;
 
-                // Rebuild the token using Spring Boot's Jwt class
-                Jwt jwt = Jwt.withTokenValue(tokenString)
-                        .headers(h -> h.putAll(jwtHeaders.toJSONObject()))
-                        .claims(c -> c.putAll(claims))
-                        .build();
+    }
+
+    @Override
+    public String createUser(UserProfile user, @Nullable String password, Boolean emailVerified, Boolean tempPassword) throws ProvenAiException {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public Optional<UserRepresentation> getUsersByUsername(String userName) {
+        return Optional.empty();
+    }
+
+    private void convertTimestampsToInstant(Map<String, Object> claims) {
+        convertToInstant(claims, "exp");
+        convertToInstant(claims, "iat");
+        convertToInstant(claims, "nbf");
+    }
 
 
-                return jwt;
-
+    private void convertToInstant(Map<String, Object> claims, String claimKey) {
+        if (claims.containsKey(claimKey)) {
+            Object timestamp = claims.get(claimKey);
+            if (timestamp instanceof Long) {
+                Instant instant = Instant.ofEpochSecond((Long) timestamp);
+                claims.put(claimKey, instant);
+            }
         }
-
-        private void convertTimestampsToInstant(Map<String, Object> claims) {
-                convertToInstant(claims, "exp");
-                convertToInstant(claims, "iat");
-                convertToInstant(claims, "nbf");
-        }
-
-
-        private void convertToInstant(Map<String, Object> claims, String claimKey) {
-                if (claims.containsKey(claimKey)) {
-                        Object timestamp = claims.get(claimKey);
-                        if (timestamp instanceof Long) {
-                                Instant instant = Instant.ofEpochSecond((Long) timestamp);
-                                claims.put(claimKey, instant);
-                        }
-                }
-        }
+    }
 
 
 }
