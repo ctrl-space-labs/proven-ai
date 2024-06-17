f0591acf01a3d1a5d736fc49e7cfdabf23afa19d
sekasx <sekas.x@gmail.com>
2024-06-17 19:27:23 +0300

    WIP #3
    - Update ProvenAI frontend to start in port 3001
    - Setup proven-ai Keycloak localhost PKCE client

--- provenai-frontend/src/configs/auth.js
+++ provenai-frontend/src/configs/auth.js
@@ -3,36 +3,36 @@
 
 const localOidcConfig = {
   authority: "https://dev.gendox.ctrlspace.dev/idp/realms/gendox-idp-dev",
-  client_id: "gendox-pkce-public-client-local",
-  redirect_uri: "http://localhost:3000/oidc-callback/",
+  client_id: "proven-pkce-public-client-local",
+  redirect_uri: "http://localhost:3001/oidc-callback/",
   response_type: "code",
   scope: "openid profile email",
-  post_logout_redirect_uri: "http://localhost:3000/login",
-  silent_redirect_uri: "http://localhost:3000/silent-renew",
+  post_logout_redirect_uri: "http://localhost:3001/login",
+  silent_redirect_uri: "http://localhost:3001/silent-renew",
   automaticSilentRenew: true,
   pkceMethod: 'S256'
 };
 
 const devOidcConfig = {
   authority: "https://dev.gendox.ctrlspace.dev/idp/realms/gendox-idp-dev",
-  client_id: "gendox-pkce-public-client-dev",
-  redirect_uri: "https://dev.gendox.ctrlspace.dev/oidc-callback/",
+  client_id: "proven-pkce-public-client-dev",
+  redirect_uri: "https://dev.provenai.ctrlspace.dev/oidc-callback/",
   response_type: "code",
   scope: "openid profile email",
-  post_logout_redirect_uri: "https://dev.gendox.ctrlspace.dev/login",
-  silent_redirect_uri: "https://dev.gendox.ctrlspace.dev/silent-renew",
+  post_logout_redirect_uri: "https://dev.provenai.ctrlspace.dev/login",
+  silent_redirect_uri: "https://dev.provenai.ctrlspace.dev/silent-renew",
   automaticSilentRenew: true,
   pkceMethod: 'S256'
 };
 
 const prodOidcConfig = {
   authority: "https://dev.gendox.ctrlspace.dev/idp/realms/gendox-idp-dev",
-  client_id: "gendox-pkce-public-client-prod",
-  redirect_uri: "https://gendox.ctrlspace.dev/oidc-callback/",
+  client_id: "proven-pkce-public-client-prod",
+  redirect_uri: "https://provenai.ctrlspace.dev/oidc-callback/",
   response_type: "code",
   scope: "openid profile email",
-  post_logout_redirect_uri: "https://gendox.ctrlspace.dev/login",
-  silent_redirect_uri: "https://gendox.ctrlspace.dev/silent-renew",
+  post_logout_redirect_uri: "https://provenai.ctrlspace.dev/login",
+  silent_redirect_uri: "https://provenai.ctrlspace.dev/silent-renew",
   automaticSilentRenew: true,
   pkceMethod: 'S256'
 };
