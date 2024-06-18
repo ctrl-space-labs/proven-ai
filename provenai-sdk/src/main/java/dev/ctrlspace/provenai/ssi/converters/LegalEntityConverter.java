36553097df48b1481ba0bd9e8b1fcecdb90c5ad6
myrto <myrto997@gmail.com>
2024-06-18 18:02:09 +0300

    WIP: 10,12 - Search API: - Added: AuditPermissionOfUseVc creation -AuditPermissionOfUse VC - AuditPermissionOfUseVc apis

--- provenai-sdk/src/main/java/dev/ctrlspace/provenai/ssi/converters/LegalEntityConverter.java
+++ provenai-sdk/src/main/java/dev/ctrlspace/provenai/ssi/converters/LegalEntityConverter.java
@@ -2,10 +2,11 @@ package dev.ctrlspace.provenai.ssi.converters;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import dev.ctrlspace.provenai.ssi.issuer.VerifiableCredentialBuilder;
 import dev.ctrlspace.provenai.ssi.model.vc.VerifiableCredential;
 import dev.ctrlspace.provenai.ssi.model.vc.id.LegalEntityCredentialSubject;
-import dev.ctrlspace.provenai.ssi.model.vc.id.NaturalPersonCredentialSubject;
 import id.walt.credentials.vc.vcs.W3CVC;
 import kotlinx.serialization.json.Json;
 import kotlinx.serialization.json.JsonElement;
@@ -13,56 +14,53 @@ import kotlinx.serialization.json.JsonObject;
 import org.json.JSONException;
 
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
-/**
- * LegalEntityConverter class is used to convert VerifiableCredential<LegalEntityCredentialSubject> to W3CVC format.
- */
 public class LegalEntityConverter {
 
-    public W3CVC convertToW3CVC(VerifiableCredential<LegalEntityCredentialSubject> legalEntityVerifiableCredential) throws JsonProcessingException, JSONException {
+    private ObjectMapper objectMapper;
 
+    public LegalEntityConverter() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    public LegalEntityConverter(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public W3CVC convertToW3CVC(VerifiableCredential<LegalEntityCredentialSubject> legalEntityCredential) throws JsonProcessingException, JSONException {
         VerifiableCredentialBuilder credentialBuilder = new VerifiableCredentialBuilder();
 
-
         // Add context and type
-        credentialBuilder.addContext("https://www.w3.org/2018/credentials/v1" );
+        credentialBuilder.addContext("https://www.w3.org/2018/credentials/v1");
         credentialBuilder.addType("VerifiableCredential");
-        credentialBuilder.addType("EBSILegalEntityVerifiableID ");
-        credentialBuilder.setIssuerDid("did:key:z4MXj1wBzi9jUstyQ9Be6tS8NnnnVfLFUbMTRmpmB8xb56XLEzhHLNUgiDSVWqZtmcambEfswNGimSwxr59jW9zLFfQjGkaXH6RghxFJxdbXrBrCA7cgh6MYF7XQw5G1ktAAY2ff7oZo2CxHBVtK5sL4WyEFivMxqtHJXCPoscVVNKgYLBuAY19Vvx45uY9GsiaLYYzFyMC9LGpc2AyuWeTrec1uqZVN9ebAZ36spoPf3AxH9yF8wtpsGkRNA5yiCQfnhskSHdQ1MyfZ9fHzxaKWvMwbgsQu1qrEQ7Rj2BLmW4pbkhJAcmBUC4D4riZPySGqeD1ieZ2okpNrD7ccuk5jdYGf7XKvxX2StGe7r5hTxKvtNhSyW");
-        credentialBuilder.setCredentialId("urn:uuid:"+ legalEntityVerifiableCredential.getId());
-//        credentialBuilder.setSubjectDid(legalEntityVerifiableCredential.getCredentialSubject().getId());
-        credentialBuilder.setSubjectDid("did:web:your-web-domain:your-web-path#wA-pJvAXGG8HlBDb1B2tPsv9V35i4LBa4XoMoPbRAqU");
-        //
-        // Set validity duration
-        Duration duration = Duration.ofDays(30); // Example: 30 days validity period
+        credentialBuilder.addType("VerifiableLegalEntity");
 
-        credentialBuilder.validFor(duration);
+        // Set credential ID, issuer DID, and subject DID
+        credentialBuilder.setCredentialId("urn:uuid:" + UUID.randomUUID());
+        credentialBuilder.setIssuerDid("did:example:issuerDid");
+        credentialBuilder.setSubjectDid(legalEntityCredential.getCredentialSubject().getId());
         Json json = Json.Default;
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonObjectString = objectMapper.writeValueAsString(legalEntityVerifiableCredential.getCredentialSubject());
+        String jsonObjectString = objectMapper.writeValueAsString(legalEntityCredential.getCredentialSubject());
         JsonElement jsonElement = json.parseToJsonElement(jsonObjectString);
-        Map<String, JsonElement> map = Map.of("LegalEntity", jsonElement);
+        Map<String, JsonElement> map = Map.of("legalEntity", jsonElement);
         JsonObject jsonObject = new JsonObject(map);
-        jsonObject.put("legalPersonIdentifier", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getLegalPersonIdentifier() + "\""));
-        jsonObject.put("legalName", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getLegalName() + "\""));
-        jsonObject.put("legalAddress", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getLegalAddress() + "\""));
-        jsonObject.put("VATRegistration", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getVATRegistration() + "\""));
-        jsonObject.put("taxReference", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getTaxReference() + "\""));
-        jsonObject.put("LEI", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getLEI() + "\""));
-        jsonObject.put("EORI", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getEORI() + "\""));
-        jsonObject.put("SEED", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getSEED() + "\""));
-        jsonObject.put("SIC", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getSIC() + "\""));
-        jsonObject.put("domainName", json.parseToJsonElement("\"" + legalEntityVerifiableCredential.getCredentialSubject().getDomainName()+ "\""));
 
-        String issuerKeyJsonString = "{\"type\":\"jwk\",\"jwk\":{\"kty\":\"OKP\",\"d\":\"mDhpwaH6JYSrD2Bq7Cs-pzmsjlLj4EOhxyI-9DM1mFI\",\"crv\":\"Ed25519\",\"kid\":\"Vzx7l5fh56F3Pf9aR3DECU5BwfrY6ZJe05aiWYWzan8\",\"x\":\"T3T4-u1Xz3vAV2JwPNxWfs4pik_JLiArz_WTCvrCFUM\"}}";
-        jsonObject.put("issuerKey", json.parseToJsonElement(issuerKeyJsonString));
         credentialBuilder.credentialSubject(jsonObject);
 
+        // Set validity duration
+        Duration duration = Duration.ofDays(365); // Example: 365 days validity period
+        credentialBuilder.validFor(duration);
+
+        // Build the credential
         W3CVC credential = credentialBuilder.buildCredential();
         return credential;
     }
-
-
 }
+
+
