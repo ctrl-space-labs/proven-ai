b4feca19f8daebbe5c2de3530b1fbe59e134c875
sekasx <sekas.x@gmail.com>
2024-06-25 20:44:57 +0300

    Fix #278 Create W3C VC verification-offer and issuance-offer
    - update default Issuer and verifier endpoints (still hardcoded)
    - implement workaround for credentialConfigurationId
    - Update AgentID VC to have a random UUID as id

--- provenai-sdk/src/main/java/dev/ctrlspace/provenai/utils/SSIConstants.java
+++ provenai-sdk/src/main/java/dev/ctrlspace/provenai/utils/SSIConstants.java
@@ -2,9 +2,9 @@ package dev.ctrlspace.provenai.utils;
 
 public class SSIConstants {
 
-    public static final String WALT_ID_ISSUER_API = "http://localhost:7002/openid4vc/jwt/issue";
+    public static final String WALT_ID_ISSUER_API = "https://proven-ai-dev.ctrlspace.dev/issuer/openid4vc/jwt/issue";
 
-    public static final String WALT_ID_VERIFIER_API = "http://localhost:7003/openid4vc/verify";
+    public static final String WALT_ID_VERIFIER_API = "https://proven-ai-dev.ctrlspace.dev/verifier/openid4vc/verify";
 
 //    public static final String VERIFIER_SUCCESS_URL = "https://example.com/success?id=$id";
 //
