f1682dd621ae0534ee5876f12a34dca42ee6e879
sekasx <sekas.x@gmail.com>
2024-07-18 17:32:36 +0300

    Deploy website - based on 479cf5aa980a9ce798d18039d6b4b2e467c93d5f

---
+++ assets/js/814f3328.d66dc4ce.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[7472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"ProvenAI 1st release","permalink":"/blog/2024/07/18/proven-ai-first-release-blog-post","unlisted":false}]}')}}]);
---
+++ assets/js/83d480e9.7fda41b1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[9650],{4078:e=>{e.exports=JSON.parse('{"label":"release","permalink":"/blog/tags/release","allTagsPath":"/blog/tags","count":1,"unlisted":false}')}}]);
