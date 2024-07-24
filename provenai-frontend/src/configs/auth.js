383a1a120a4c30cb4f5df7d57c5825578177c33f
Giannis Yfantidis <96373640+gyfantidis@users.noreply.github.com>
2024-07-24 17:58:34 +0300

    WIX ticket # Frond end environment variables
      - Create front end s environment variables

--- provenai-frontend/src/configs/auth.js
+++ provenai-frontend/src/configs/auth.js
@@ -2,14 +2,26 @@
 
 
 
+// const oidcConfig = {
+//   authority: process.env.REACT_APP_OIDC_AUTHORITY,
+//   client_id: process.env.REACT_APP_OIDC_CLIENT_ID,
+//   redirect_uri: process.env.REACT_APP_OIDC_REDIRECT_URI,
+//   response_type: "code",
+//   scope: "openid profile email",
+//   post_logout_redirect_uri: process.env.REACT_APP_OIDC_POST_LOGOUT_REDIRECT_URI,
+//   silent_redirect_uri: process.env.REACT_APP_OIDC_SILENT_REDIRECT_URI,
+//   automaticSilentRenew: true,
+//   pkceMethod: 'S256'
+// };
+
 const oidcConfig = {
-  authority: process.env.REACT_APP_OIDC_AUTHORITY,
-  client_id: process.env.REACT_APP_OIDC_CLIENT_ID,
-  redirect_uri: process.env.REACT_APP_OIDC_REDIRECT_URI,
+  authority: "https://dev.gendox.ctrlspace.dev/idp/realms/gendox-idp-dev",
+  client_id: "proven-pkce-public-client-local",
+  redirect_uri: "http://localhost:3001/oidc-callback/",
   response_type: "code",
   scope: "openid profile email",
-  post_logout_redirect_uri: process.env.REACT_APP_OIDC_POST_LOGOUT_REDIRECT_URI,
-  silent_redirect_uri: process.env.REACT_APP_OIDC_SILENT_REDIRECT_URI,
+  post_logout_redirect_uri: "http://localhost:3001/login",
+  silent_redirect_uri: "http://localhost:3001/silent-renew/",
   automaticSilentRenew: true,
   pkceMethod: 'S256'
 };
