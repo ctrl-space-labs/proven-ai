b0cfbc939972dfc5352b533cadef2c9bf0713920
Giannis Yfantidis <96373640+gyfantidis@users.noreply.github.com>
2024-07-26 15:23:12 +0300

    WIP ticket # Update CredentialsQrCode Card
      - Update CredentialsQrCode Card

--- provenai-frontend/src/views/provenAI/registration-components/CredentialsWithQrCodeComponent.js
+++ provenai-frontend/src/views/provenAI/registration-components/CredentialsWithQrCodeComponent.js
@@ -18,16 +18,18 @@ import Icon from "src/@core/components/icon";
 import QRCodeComponent from "src/views/provenAI/gq-code-component/QRCodeComponent";
 import useClipboard from "../../../@core/hooks/useClipboard";
 
-const CredentialsWithQrCodeComponent = ({ handleCredentialsClose,
-                                            getURL,
-                                        title}) => {
+const CredentialsWithQrCodeComponent = ({
+  handleCredentialsClose,
+  getURL,
+  title,
+}) => {
   const theme = useTheme();
 
-  const clipboard = useClipboard()
+  const clipboard = useClipboard();
 
   const [url, setUrl] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [copyButtonText, setCopyButtonText] = useState('Copy URL')
+  const [copyButtonText, setCopyButtonText] = useState("Copy URL");
 
   useEffect(() => {
     const fetchURL = async () => {
@@ -38,36 +40,36 @@ const CredentialsWithQrCodeComponent = ({ handleCredentialsClose,
     };
 
     fetchURL();
-
   }, []);
 
-    const copyValue = (value) => {
-        clipboard.copy(value)
-        // toast.success('The source code has been copied to your clipboard.', {
-        //     duration: 2000
-        // })
-    }
+  const copyValue = (value) => {
+    clipboard.copy(value);
+    // toast.success('The source code has been copied to your clipboard.', {
+    //     duration: 2000
+    // })
+  };
 
-    function copyUrl() {
-        return () => {
-            copyValue(url)
-            setCopyButtonText('Copied!')
-        };
-    }
+  function copyUrl() {
+    return () => {
+      copyValue(url);
+      setCopyButtonText("Copied!");
+    };
+  }
 
-    return (
+  return (
     <Box
       sx={{
-         border: "2px solid ",
-         borderColor: "primary.main",
-         borderRadius: "2px",
-         backgroundColor: "#FFFFFF",
-      }}>
+        border: "2px solid ",
+        borderColor: "primary.main",
+        borderRadius: "2px",
+        backgroundColor: "#FFFFFF",
+      }}
+    >
       <Box
         sx={{
           display: "flex",
           justifyContent: "space-between",
-          alignItems: "center",          
+          alignItems: "center",
           px: { xs: theme.spacing(5), sm: theme.spacing(15) },
           pt: { xs: theme.spacing(5), sm: theme.spacing(8) },
         }}
@@ -78,19 +80,20 @@ const CredentialsWithQrCodeComponent = ({ handleCredentialsClose,
             textAlign: "left",
             fontSize: "1.5rem",
             padding: 0,
-            color: theme.palette.primary.dark,         
+            color: theme.palette.primary.dark,
           }}
         >
-            {title}
+          {title}
         </DialogTitle>
-        {title !== "Receive your Agent ID Credential" && (
-          <IconButton
-            sx={{ color: "primary.main" }}
-            onClick={handleCredentialsClose}
-          >
-            <Icon icon="mdi:close" />
-          </IconButton>
-        )}
+        {title !== "Receive your Agent ID Credential" &&
+          title !== "Receive your Data Ownership Credential" && (
+            <IconButton
+              sx={{ color: "primary.main" }}
+              onClick={handleCredentialsClose}
+            >
+              <Icon icon="mdi:close" />
+            </IconButton>
+          )}
       </Box>
 
       <DialogContent
@@ -105,16 +108,16 @@ const CredentialsWithQrCodeComponent = ({ handleCredentialsClose,
       >
         <Box sx={{ textAlign: "center" }}>
           {/*  if offerURL is null show a loader else show the QRCodeComponent*/}
-            {loading ? (
-                <Typography variant="h6">Loading...</Typography>
-            ) : (
-                <QRCodeComponent
-                value={url}
-                size={256}
-                fgColor={theme.palette.primary.light}
-                logo="/images/provenAILogo.svg"
-                />
-            )}
+          {loading ? (
+            <Typography variant="h6">Loading...</Typography>
+          ) : (
+            <QRCodeComponent
+              value={url}
+              size={256}
+              fgColor={theme.palette.primary.light}
+              logo="/images/provenAILogo.svg"
+            />
+          )}
         </Box>
       </DialogContent>
       <DialogActions
@@ -127,19 +130,20 @@ const CredentialsWithQrCodeComponent = ({ handleCredentialsClose,
         }}
       >
         <Button variant="outlined" onClick={copyUrl()}>
-            <Icon icon='mdi:content-copy' fontSize={20} />
-            {copyButtonText}
+          <Icon icon="mdi:content-copy" fontSize={20} />
+          {copyButtonText}
         </Button>
 
-        {title !== "Receive your Agent ID Credential" && (
-          <Button
-            variant="contained"
-            sx={{ mr: 2 }}
-            onClick={handleCredentialsClose}
-          >
-            I don't have wallet :'(
-          </Button>
-        )}
+        {title !== "Receive your Agent ID Credential" &&
+          title !== "Receive your Data Ownership Credential" && (
+            <Button
+              variant="contained"
+              sx={{ mr: 2 }}
+              onClick={handleCredentialsClose}
+            >
+              I don't have wallet :'(
+            </Button>
+          )}
       </DialogActions>
     </Box>
   );
