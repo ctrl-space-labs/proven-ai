4dd32249504c10b1836c89b62ec9d309894330d6
myrto <myrto997@gmail.com>
2024-10-06 20:20:01 +0300

    - issuer documentaiton - updated java docs

--- provenai-sdk/src/main/java/dev/ctrlspace/provenai/ssi/model/vc/attestation/DataOwnershipCredentialSubject.java
+++ provenai-sdk/src/main/java/dev/ctrlspace/provenai/ssi/model/vc/attestation/DataOwnershipCredentialSubject.java
@@ -9,6 +9,14 @@ import lombok.NoArgsConstructor;
 import java.time.Instant;
 import java.util.List;
 
+/**
+ * Class to represent the 'credentialSubject' part of the JSON
+ * This class represents the credential subject of a Data Ownership Credential Subject.
+ * <p>
+ *     This class is compliant with the W3C and EBSI v.2 Data Model of Verifiable Credentials specifications.
+ *     @see <a href="https://code.europa.eu/ebsi/json-schema/-/tree/main/schemas/vcdm2.0/attestation">EBSI v.2.0 Attestation</a>
+ * </p>
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
